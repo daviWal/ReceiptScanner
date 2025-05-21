7d5c227bc27d31600e2edfafccb4f64266b0e3ec
David Walitza <david.walitza@icloud.com>
2025-05-21 11:14:35 +0200

    Initial Commit

---
+++ BillScanner/BillScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillScannerApp.swift
+//  BillScanner
+//
+//  Created by David Walitza on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BillScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillScannerTests/BillScannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  BillScannerTests.swift
+//  BillScannerTests
+//
+//  Created by David Walitza on 21.05.2025.
+//
+
+import Testing
+
+struct BillScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
